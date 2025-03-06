1ceebd2499adc3f5c6a75f7037cf0a42f55a55ef
Andreastak86 <andreas.takvam@gmail.com>
2025-03-06 11:16:54 +0100

    change margin

--- app/page.js
+++ app/page.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Page() {
     return (
-        <div>
-            <h1>Page</h1>
+        <div className='m-6'>
+            <h1>My homepage</h1>
+            <p>This is a page about me</p>
         </div>
     );
 }
